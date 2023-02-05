51a3cf0edb9bdb52f7b657def998a8f6fda96e72
jahanbaev <rracerov@gmail.com>
2023-02-05 18:46:03 +0500

    test: test

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const { app, BrowserWindow } = require('electron')
+const {ipcMain, app, BrowserWindow } = require('electron')
 const AutoLaunch = require('auto-launch');
-require('./services/server.js')
+const chokidar = require('chokidar');
+const ipcRenderer = require('electron').ipcRenderer;
+require('./services/server.js');
+
+
+
+ipcRenderer.on('store-data', function (event,store) {
+    console.log(store);
+});
+
 const autoLauncher = new AutoLaunch({
     name: "MyApp"
 });
@@ -18,14 +27,30 @@ const createWindow = () => {
     // skipTaskbar: true
 })
 
+
   win.maximize()
   win.setFullScreen(true);
-  win.loadFile('index.html')
+  win.loadFile('index.html');
   setTimeout(()=>{
     // win.hide()
   },4000)
 }
 
+const litenFs = () =>{
+    let watcher = chokidar.watch('/Users', {ignored: /^\./, persistent: true});
+
+    watcher
+      .on('add', function(path) {
+        if(path.includes(".pptx")){
+        console.log('File', path, 'has been added');
+      }
+      })
+      // .on('change', function(path) {console.log('File', path, 'has been changed');})
+      // .on('unlink', function(path) {console.log('File', path, 'has been removed');})
+      // .on('error', function(error) {console.error('Error happened', error);})
+}
+
 app.whenReady().then(() => {
+  // litenFs()
   createWindow()
 })
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,27 @@
+const {ipcRenderer} = require('electron')
 let list = [];
-let element = document.querySelector(".grid")
+let element = document.querySelector(".grid");
+let closeButton = document.querySelector("#close")
+
+closeButton.onclick = () =>{
+    closeButton.parentElement.classList.add("hidden")
+}
+
+
+storeWindow.webContents.send('store-data', store);
+
 firebase.database().ref("it-park-active").on("child_added", function(snapshot) {
-    console.log(snapshot.val())
     let node = document.createElement("div")
     node.innerHTML = `
     <h1>${snapshot.val().student.slice(0,1)}</h1>
     <h2>${snapshot.val().student}</h2>
-    `
+    `;
+    node.onclick = ()=>{
+        closeButton.parentElement.classList.remove("hidden")
+    }
     element.appendChild(node)
 });
+
 
 firebase.database().ref("it-park-active").on("child_removed", function(snapshot) {
     console.log(snapshot.val(), "removed")
